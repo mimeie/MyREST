7571830f215cf9184f40de1e780737f73a52c68a
Michael Meienberg <mei@acs-ag.ch>
2020-07-27 00:02:58 +0200

    änderung triggern

--- Startup.cs
+++ Startup.cs
@@ -19,6 +19,7 @@ namespace MyRest
             Configuration = configuration;
         }
 
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
